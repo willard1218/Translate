da4692a60172332d82a6664db662acaaec746332
Willard.Lin <willard1218@gmail.com>
2017-04-02 22:38:24 +0800

    modify WLQuiz relationship : diffWords ordered

--- Translate/Database/WLQuiz+CoreDataProperties.h
+++ Translate/Database/WLQuiz+CoreDataProperties.h
@@ -2,7 +2,7 @@
 //  WLQuiz+CoreDataProperties.h
 //  Translate
 //
-//  Created by willard on 2017/4/1.
+//  Created by willard on 2017/4/2.
 //  Copyright © 2017年 willard. All rights reserved.
 //
 
@@ -20,17 +20,23 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nullable, nonatomic, copy) NSString *answer;
 @property (nullable, nonatomic, copy) NSString *userAnswer;
 @property (nullable, nonatomic, copy) NSString *question;
-@property (nullable, nonatomic, retain) NSSet<WLDiffWord *> *diffWords;
+@property (nullable, nonatomic, retain) NSOrderedSet<WLDiffWord *> *diffWords;
 @property (nullable, nonatomic, retain) WLArticle *article;
 
 @end
 
 @interface WLQuiz (CoreDataGeneratedAccessors)
 
+- (void)insertObject:(WLDiffWord *)value inDiffWordsAtIndex:(NSUInteger)idx;
+- (void)removeObjectFromDiffWordsAtIndex:(NSUInteger)idx;
+- (void)insertDiffWords:(NSArray<WLDiffWord *> *)value atIndexes:(NSIndexSet *)indexes;
+- (void)removeDiffWordsAtIndexes:(NSIndexSet *)indexes;
+- (void)replaceObjectInDiffWordsAtIndex:(NSUInteger)idx withObject:(WLDiffWord *)value;
+- (void)replaceDiffWordsAtIndexes:(NSIndexSet *)indexes withDiffWords:(NSArray<WLDiffWord *> *)values;
 - (void)addDiffWordsObject:(WLDiffWord *)value;
 - (void)removeDiffWordsObject:(WLDiffWord *)value;
-- (void)addDiffWords:(NSSet<WLDiffWord *> *)values;
-- (void)removeDiffWords:(NSSet<WLDiffWord *> *)values;
+- (void)addDiffWords:(NSOrderedSet<WLDiffWord *> *)values;
+- (void)removeDiffWords:(NSOrderedSet<WLDiffWord *> *)values;
 
 @end
 
