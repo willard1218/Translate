47a98de73813c092c52d21d32f4af6bb954e144e
Willard.Lin <willard1218@gmail.com>
2017-05-04 08:13:38 +0800

    增加 localize 方法

---
+++ Translate/Category/NSString+Helpers.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Helpers.h
+//  Translate
+//
+//  Created by willard on 2017/5/4.
+//  Copyright © 2017年 willard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Helpers)
+
+@end
---
+++ Translate/Category/NSString+Helpers.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Helpers.m
+//  Translate
+//
+//  Created by willard on 2017/5/4.
+//  Copyright © 2017年 willard. All rights reserved.
+//
+
+#import "NSString+Helpers.h"
+
+@implementation NSString (Helpers)
+
+@end
